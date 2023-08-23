c3df5d23b488c93c5e75b5f51b18d18f1179c2af
Legday <chlwodud0327@naver.com>
2023-08-23 21:08:40 +0900

    Board Domain 오류코드 수정

--- src/main/java/com/legday/simpleboard/domain/Board.java
+++ src/main/java/com/legday/simpleboard/domain/Board.java
@@ -1,7 +1,6 @@
 package com.legday.simpleboard.domain;
 
 import com.legday.simpleboard.dto.UpdateBoardDto;
-import com.legday.simpleboard.dto.WriteDto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
